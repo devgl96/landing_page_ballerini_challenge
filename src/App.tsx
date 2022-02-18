0f1edb5808a8a3582851389187fe22e1b8be0fdc
devgl96 <georgelucas.dev@gmail.com>
2022-02-17 23:14:19 -0300

    Main Page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerMain">
+      <Header />
+      
+      <div className="contentMain">
+        <div className="left">
+          <h2>O maior banco de devs do Brasil</h2>
+          <p>Nao importa se front ou back end,  fazer networking e muito importante. Faça parte da maior comunidade de desenvolvedores brasileiros.</p>
+          <div className="buttonArea">
+            <button>Entre agora</button>
+            <hr />
+          </div>
+        </div>
+        <div className="right">
+          <img src="/images/Programador_main.svg" alt="Programador feliz sentado no puffy" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,32 @@
+import "./styles.css";
+
 export function Header() {
-  return <h1>Header</h1>;
+  const pageTable = false; 
+
+  return (
+    <div className="container">
+      <div className="socialMedias">
+        <img src="/images/linkedin.svg" alt="Linkedin icon" />
+        <img src="/images/facebook.svg" alt="Facebook icon" />
+        <img src="/images/discord.svg" alt="Discord icon" />
+      </div>
+      <div className="logo">
+        <img src="/images/Logo.svg" alt="Logo ballerini devs" />
+      </div>
+      <div className="searchAddSection">
+      {pageTable && 
+        <>
+          <div className="inputSearch">
+            <img src="/images/feather_search.svg" alt="icon feather search" />
+            <input 
+              type="text" 
+              placeholder="Buscar"
+            />
+          </div>
+          <button>Adicionar Desenvolvedor</button>
+        </>
+      }
+      </div>
+    </div>
+  );
 }
