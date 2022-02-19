2b7dbba31192a6ec6005326a2880f75c75600279
devgl96 <georgelucas.dev@gmail.com>
2022-02-19 01:06:42 -0300

    Completed the Landing page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles.css';
+import { Header } from '../../components/Header';
+
+import { useNavigate } from "react-router-dom";
+
+export function Landing() {
+  const navigate = useNavigate();
+
+  function nextPage() {
+    navigate("/TableDevs");
+  }
+  return (
+    <div className="containerMain">
+      <Header isTableDev={false} />
+      
+      <div className="contentMain">
+        <div className="left">
+          <h2>O maior banco de devs do Brasil</h2>
+          <p>Nao importa se front ou back end,  fazer networking e muito importante. Faça parte da maior comunidade de desenvolvedores brasileiros.</p>
+          <div className="buttonArea">
+            <button onClick={nextPage}>Entre agora</button>
+            <hr />
+          </div>
+        </div>
+        <div className="right">
+          <img src="/images/Programador_main.svg" alt="Programador feliz sentado no puffy" />
+        </div>
+      </div>
+    </div>
+  );
+};
