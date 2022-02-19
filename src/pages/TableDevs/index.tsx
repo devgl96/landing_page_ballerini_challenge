d696822ddb852ff44119581f2fc56f41389cc7d1
devgl96 <georgelucas.dev@gmail.com>
2022-02-19 01:07:05 -0300

    Some setting is completed in tabledevs

--- src/pages/TableDevs/index.tsx
+++ src/pages/TableDevs/index.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { DevCard } from "../../components/DevCard";
+import { Header } from "../../components/Header";
+import useDevelopers from "../../hooks/useDevelopers";
+
+import "./styles.css";
+
+interface DevProps {
+  name: string;
+  avatar: string;
+  cargo: string;
+  github: string;
+  linkedin: string;
+}
+
 export function TableDevs() {
+  const { developers } = useDevelopers();
+  
   return(
-    <h1>TableDevs</h1>
+    <div className="containerMain">
+      <Header isTableDev={true} />
+      <div className="contentMainDev">
+        <img src="/images/botaoLeft.svg" alt="right button to change" />
+        <div className="sectionDev">
+          {developers.length > 0 ? 
+            <>
+                {developers.map((developer) => (
+                  <DevCard key={developer.id} developer={developer}/>
+                  ))}
+            </>
+            :
+            <h1>Sem desenvolvedores no momento!</h1>
+          }
+          </div>
+          <img src="/images/botaoRight.svg" alt="left button to change" />
+      </div>
+    </div>
   );
 }
